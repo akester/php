b5126fef763d67cff0ada8efbc3dcfc310118bd4
Andrew Kester <aikester@aikester.com>
2026-04-06 12:58:24 -0500

    Update cherry to just merge

--- cherry.sh
+++ cherry.sh
@@ -3,8 +3,8 @@
 set -e
 set -x
 
-git checkout 8.1 && git cherry-pick $1 && git push && git checkout latest
-git checkout 8.2 && git cherry-pick $1 && git push && git checkout latest
-git checkout 8.3 && git cherry-pick $1 && git push && git checkout latest
-git checkout 8.4 && git cherry-pick $1 && git push && git checkout latest
-git checkout 8.5 && git cherry-pick $1 && git push && git checkout latest
+git checkout 8.1 && git merge latest && git push && git checkout latest
+git checkout 8.2 && git merge latest && git push && git checkout latest
+git checkout 8.3 && git merge latest && git push && git checkout latest
+git checkout 8.4 && git merge latest && git push && git checkout latest
+git checkout 8.5 && git merge latest && git push && git checkout latest
